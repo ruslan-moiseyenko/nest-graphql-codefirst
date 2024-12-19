b3c4bbfb6e85c380a008e869d44de3cc9a80611f
Ruslan Moiseyenko <50419466+ruslan-moiseyenko@users.noreply.github.com>
2024-12-19 11:55:18 +0200

    added unions

--- src/drinks/drinks.resolver.ts
+++ src/drinks/drinks.resolver.ts
@@ -1,12 +1,13 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Coffee } from 'src/coffees/entities/coffee.entity/coffee.entity';
-import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { DrinksResultUnion } from 'src/common/union/drinks-result.union';
 import { Tea } from 'src/teas/entities/tea.entity/tea.entity';
 
 @Resolver()
 export class DrinksResolver {
-  @Query(() => [Drink], { name: 'drinks' })
-  async getAllDrinks(): Promise<Drink[]> {
+  @Query(() => [DrinksResultUnion], { name: 'drinks' })
+  async getAllDrinks(): Promise<(typeof DrinksResultUnion)[]> {
+    // mock data
     const coffee = new Coffee();
     coffee.id = 1;
     coffee.name = 'Colombian';
