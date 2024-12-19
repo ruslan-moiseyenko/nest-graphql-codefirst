34f07af0b825f8d0a1a2126c06d2aa097aea8a31
Ruslan Moiseyenko <50419466+ruslan-moiseyenko@users.noreply.github.com>
2024-12-19 12:32:51 +0200

    added simple middleware

--- src/coffees/entities/coffee.entity/coffee.entity.ts
+++ src/coffees/entities/coffee.entity/coffee.entity.ts
@@ -2,6 +2,7 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Flavor } from 'src/coffees/entities/flavor.entity/flavor.entity';
 import { CoffeeType } from 'src/common/enums/coffee-type.enum';
 import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { loggerMiddleware } from 'src/common/middleware/logger.middleware';
 import {
   Column,
   CreateDateColumn,
@@ -18,6 +19,7 @@ export class Coffee implements Drink {
   @Field(() => ID, { description: 'The id of the coffee' })
   id: number;
 
+  @Field({ middleware: [loggerMiddleware] })
   @Column()
   name: string;
 
